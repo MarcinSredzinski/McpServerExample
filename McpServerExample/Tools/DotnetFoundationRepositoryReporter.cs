b553f1f29b4b062f822bc8b9908658cecdcc14b6
m.sredzinski94 <m.sredzinski94@gmail.com>
2025-08-16 13:56:24 +0200

    Add project files.

---
+++ McpServerExample/Tools/DotnetFoundationRepositoryReporter.cs
@@ -0,0 +1,23 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+
+namespace McpServerExample.Tools;
+
+[McpServerToolType]
+internal class DotnetFoundationRepositoryReporter
+{
+    [McpServerTool(Name = "GetDotnetFoundationRepositories")]
+    [Description("Fetches repositories from the .NET Foundation GitHub organization.")]
+    internal async Task<string> GetDotnetFoundationRepositories()
+    {
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Add("User-Agent", "McpServerExample");
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+        var json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+        return json;
+    }
+}
